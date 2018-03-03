647674fa5479ab5a6a4eed5f216e62328cf5762b
Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>
2018-03-03 12:44:25 +0000

    pymatreader added

---
+++ pymatreader.py
@@ -0,0 +1,126 @@
+import h5py
+import numpy
+import scipy.io
+
+__all__ = 'read_mat'
+
+"""
+This is a small module intended to facilitate reading .mat files containing large data structures into python.
+"""
+
+
+def read_mat(filename, variable_names = None, ignore_fields=[]):
+
+    """This function reads .mat files of version <7.3 or 7.3 and returns the contained data structure
+    as a dictionary of nested substructure similar to scipy.io.loadmat style.
+
+    Parameters
+    ----------
+    filename: str
+        Path and filename of the .mat file containing the data.
+    variable_names: str, optional
+        Reads only the data contained in the specified dict key or variable name. Default is None.
+    ignore_fields: list of strings, optional
+        Ignores every dict key/variable name specified in the list within the entire structure. Only works for .mat files
+        v 7.3. Default is [].
+
+    Returns
+    -------
+    dict
+        A structure of nested dictionaries, with variable names as keys and variable data as values.
+    """
+
+    try:
+        hdf5_file = scipy.io.loadmat(filename, struct_as_record=False, squeeze_me=True, variable_names=variable_names)
+        data = _check_keys(hdf5_file)
+        print "finished loading .mat file <v7.3"
+    except NotImplementedError:
+        hdf5_file = h5py.File(filename, "r")
+        data = _browse(hdf5_file, hdf5_file, variable_names=variable_names, ignore_fields=ignore_fields)
+        hdf5_file.close()
+        print "finished loading .mat file v7.3"
+    return data
+
+
+
+
+def _browse(struct, hdf5_file, variable_names = None, ignore_fields=[]):
+
+    """private function which runs through h5py structure recursively, creating subdicts for every substructure.
+    calls _browse_dataset() to extract values"""
+    
+    copy = {}
+    for key, value in zip(struct, struct.values()):
+        if key not in ignore_fields and (not variable_names or key in variable_names) and key != '#refs#':
+            if type(value) == h5py._hl.group.Group:
+                copy[key] = _browse(value, hdf5_file, ignore_fields=ignore_fields)
+            else:
+                copy[key] = _browse_dataset(value, hdf5_file)
+    return copy
+
+
+def _browse_dataset(struct, hdf5_file):
+
+    """private function, which recursively browses through h5py Dataset to extract values. Calls _assign_types()
+    to assign data types"""
+    
+    if type(struct) in (h5py._hl.dataset.Dataset,numpy.ndarray):
+        if struct.size > 1:
+            content = numpy.squeeze(struct).T
+            for ind,val in enumerate(content):
+                content[ind] = _browse_dataset(val,hdf5_file)
+        elif type(struct) == h5py._hl.dataset.Dataset:
+            content = _browse_dataset(struct.value,hdf5_file)
+        elif type(struct) == numpy.ndarray:
+            content = _browse_dataset(struct[0], hdf5_file)
+    elif type(struct) == h5py.h5r.Reference:
+        content = hdf5_file[struct].value.T
+    else:
+        content = struct
+    return _assign_types(content)
+
+
+def _assign_types(values):
+
+    """private function, which assigns correct types to h5py extracted values from _browse_dataset()"""
+    
+    if type(values) == numpy.ndarray:
+        values = numpy.squeeze(values)
+        if values.dtype in ("uint8","uint16","uint32"):
+            if values.size > 1:
+                assigned_values = u''.join(unichr(c) for c in values)
+            else:
+                assigned_values = unichr(values)
+        else:
+            assigned_values = values
+    elif type(values) == numpy.float64:
+        assigned_values = float(values)
+    else:
+        assigned_values = values
+    return assigned_values
+    
+    
+def _check_keys(data_dict):
+
+    """private function to enhance scipy.io.loadmat. checks if entries in dictionary are mat-objects.
+    If yes _todict is called to change them to nested dictionaries"""
+    
+    for key in data_dict:
+        if isinstance(data_dict[key], scipy.io.matlab.mio5_params.mat_struct):
+            data_dict[key] = _todict(data_dict[key])
+    return data_dict
+
+
+def _todict(matobj):
+
+    """private function to enhance scipy.io.loadmat. 
+    A recursive function which constructs from matobjects nested dictionaries"""
+    
+    data_dict = {}
+    for strg in matobj._fieldnames:
+        elem = matobj.__dict__[strg]
+        if isinstance(elem, scipy.io.matlab.mio5_params.mat_struct):
+            data_dict[strg] = _todict(elem)
+        else:
+            data_dict[strg] = elem
+    return data_dict
