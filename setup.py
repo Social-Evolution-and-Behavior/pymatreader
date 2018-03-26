caf436b95bf74710f1fd74adb997659c6e47585b
Thomas Hartmann <thomas.hartmann@th-ht.de>
2018-03-26 18:39:27 +0200

    added long description

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
     url='https://gitlab.com/obob/pymatreader',
     license='GPL3',
     author='Dirk Gütlin & Thomas Hartmann',
-    author_email='',
+    author_email='thomas.hartmann@th-ht.de',
     description='Convenient reader for Matlab mat files',
+    long_description='Convenient reader for Matlab mat files. This package works with the <= 7 and 7.3 (i.e. HDF5) versions. A dictionary is returned, vectors and matrices are converted to numpy arrays.',
     install_requires=REQUIRED,
 )
