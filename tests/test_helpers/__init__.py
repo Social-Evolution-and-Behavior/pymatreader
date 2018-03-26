75a1734e78c6b7338fb550f37ed037c334dcca3a
Thomas Hartmann <thomas.hartmann@th-ht.de>
2018-03-26 14:40:28 +0200

    added test_helpers

---
+++ tests/test_helpers/__init__.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2018, Dirk Gütlin & Thomas Hartmann
+#
+# This file is part of the pymatreader Project, see: https://gitlab.com/obob/pymatreader
+#
+# This code has been adapted from https://github.com/larsbutler/oq-engine/blob/master/tests/utils/helpers.py
+#
+#    pymatreader is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    pymatreader is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with obob_subjectdb. If not, see <http://www.gnu.org/licenses/>.
+
+import numpy
+from nose.tools import assert_almost_equal, assert_equal
+
+def assertDeepAlmostEqual(expected, actual, *args, **kwargs):
+    """
+    Assert that two complex structures have almost equal contents.
+
+    Compares lists, dicts and tuples recursively. Checks numeric values
+    using test_case's :py:meth:`unittest.TestCase.assertAlmostEqual` and
+    checks all other values with :py:meth:`unittest.TestCase.assertEqual`.
+    Accepts additional positional and keyword arguments and pass those
+    intact to assertAlmostEqual() (that's how you specify comparison
+    precision).
+    """
+    is_root = not '__trace' in kwargs
+    trace = kwargs.pop('__trace', 'ROOT')
+    try:
+        if isinstance(expected, (int, float, complex)):
+            assert_almost_equal(expected, actual, *args, **kwargs)
+        elif isinstance(expected, (list, tuple, numpy.ndarray)):
+            assert_equal(len(expected), len(actual))
+            for index in range(len(expected)):
+                v1, v2 = expected[index], actual[index]
+                assertDeepAlmostEqual(v1, v2,
+                                      __trace=repr(index), *args, **kwargs)
+        elif isinstance(expected, dict):
+            assert_equal(set(expected), set(actual))
+            for key in expected:
+                assertDeepAlmostEqual(expected[key], actual[key],
+                                      __trace=repr(key), *args, **kwargs)
+        else:
+            assert_equal(expected, actual)
+    except AssertionError as exc:
+        exc.__dict__.setdefault('traces', []).append(trace)
+        if is_root:
+            trace = ' -> '.join(reversed(exc.traces))
+            exc = AssertionError("%s\nTRACE: %s" % (exc.message, trace))
+        raise exc
