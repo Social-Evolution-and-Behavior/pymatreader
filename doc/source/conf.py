c00bfdc27173a54aa8e3f01cf5dec73f5256ccf5
Thomas Hartmann <thomas.hartmann@th-ht.de>
2018-03-26 18:10:10 +0200

    put version in sphinx config

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Dirk Gütlin & Thomas Hartmann'
 author = 'Dirk Gütlin & Thomas Hartmann'
 
 # The short X.Y version
-version = ''
+version = '0.0.1'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
