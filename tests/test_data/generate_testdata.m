c8011e902813660441859b6732f45a775bea87d2
Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>
2018-08-07 07:49:39 +0000

    Replaced ther generate_testdata.m to also contain data like cells within cells etc.

--- tests/test_data/generate_testdata.m
+++ tests/test_data/generate_testdata.m
@@ -43,41 +43,58 @@ a_float = rand(1);
 a_second_float = rand(1);
 
 a_matrix = rand(100, 100);
+a_second_matrix = rand(10,10);
 
-a_cell_array = {};
-for idx_cells = 1:100
-  a_cell_array{idx_cells} = rand(2, 5);
+a_cell_array = cell(4,1);
+for idx_cells = 1:4
+  a_cell_array{idx_cells} = rand(2, 2);
 end %for
 clear idx_cells
-
-a_struct = {};
-a_struct.string = random_string(20);
-a_struct.int = randi(255, 1);
-a_struct.float = rand(1);
-a_struct.matrix = rand(100, 100);
-a_struct.a_cell_array = {};
-for idx_cells = 1:100
-  a_struct.a_cell_array{idx_cells} = rand(2, 5);
-end %for
-clear idx_cells
-a_struct.a_cell_struct_array = {};
-for idx_cells = 1:5
-  a_struct.a_cell_struct_array{idx_cells}.int = randi(255, 1);
-  a_struct.a_cell_struct_array{idx_cells}.float = rand(1);
-  a_struct.a_cell_struct_array{idx_cells}.matrix = rand(100, 100);
-  a_struct.a_cell_struct_array{idx_cells}.string = random_string(20);
+a_second_cell_array = cell(1,4);
+for idx_cells = 1:4
+  a_second_cell_array{idx_cells} = rand(2, 2);
 end %for
 clear idx_cells
 
-a_struct.second_level.string = random_string(20);
-a_struct.second_level.int = randi(255, 1);
-a_struct.second_level.float = rand(1);
-a_struct.second_level.matrix = rand(100, 100);
-a_struct.second_level.a_cell_array = {};
-for idx_cells = 1:100
-  a_struct.second_level.a_cell_array{idx_cells} = rand(2, 5);
-end %for
-clear idx_cells
+a_struct = struct('a_field',random_string(2),'a_second_field',randi(2,2,2));
+a_second_struct = struct('a_field',random_string(2),'a_second_field',rand(2,2));
+
+%a_string_matrix = [random_string(10); random_string(10)];
+an_integer_matrix = randi(10,2,2);
+
+a_heading_cell_array = cell(6,1);
+a_heading_cell_array{1} = a_string;
+a_heading_cell_array{2} = an_integer;
+a_heading_cell_array{3} = a_float;
+a_heading_cell_array{4} = a_matrix;
+a_heading_cell_array{5} = a_cell_array;
+a_heading_cell_array{6} = a_struct;
+
+a_second_heading_cell_array = cell(1,6);
+a_second_heading_cell_array{1} = a_second_string;
+a_second_heading_cell_array{2} = a_second_integer;
+a_second_heading_cell_array{3} = a_second_float;
+a_second_heading_cell_array{4} = a_second_matrix;
+a_second_heading_cell_array{5} = a_second_cell_array;
+a_second_heading_cell_array{6} = a_second_struct;
+
+
+a_heading_struct = struct();
+a_heading_struct.string = a_string;
+a_heading_struct.integer = an_integer;
+a_heading_struct.float = a_float;
+a_heading_struct.matrix = a_matrix;
+a_heading_struct.cell = a_cell_array;
+a_heading_struct.struct = a_struct;
+
+a_second_heading_struct = struct();
+a_second_heading_struct.string = a_second_string;
+a_second_heading_struct.integer = a_second_integer;
+a_second_heading_struct.float = a_second_float;
+a_second_heading_struct.matrix = a_second_matrix;
+a_second_heading_struct.cell = a_second_cell_array;
+a_second_heading_struct.struct = a_second_struct;
+
 
 save('v6.mat', '-v6')
 save('v7.mat', '-v7')
